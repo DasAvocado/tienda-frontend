34c0a4fdc5377440c77b118c6931ec790a109783
DasAvocado <aknittel@duocuc.cl>
2026-09-09 19:27:31 -0300

    Fix: actualizar redirectUri de Azure AD a IP de produccion

--- src/app/auth-config.ts
+++ src/app/auth-config.ts
@@ -1,10 +1,10 @@
 import { Configuration, BrowserCacheLocation } from '@azure/msal-browser';
-
+//cambiar redirectUri por la ip del url cada vez que se inicia
 export const msalConfig: Configuration = {
   auth: {
     clientId: '39a9591b-35f1-4165-8cf2-667a2c643920',
     authority: 'https://login.microsoftonline.com/d276cca8-e687-4f20-8984-601387d11401',
-    redirectUri: 'http://localhost:4200'
+    redirectUri: 'http://184.73.48.53/'
   },
   cache: {
     cacheLocation: BrowserCacheLocation.LocalStorage
